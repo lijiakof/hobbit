4fe5f337b4da6981169c6901feb2fab0732cdbce
Jay Lee <lijiakof@163.com>
2012-12-16 16:15:59 +0800

    Initiative define framework

--- xhobbit.js
+++ xhobbit.js
@@ -1,80 +1,213 @@
 ﻿/// <reference path="jquip.js" />
-
 $(function () {
-	$h.initialize();
-    $h.loader.show();
- });
-
-$h = {
+    $H.autoPageHeight();
+});
+$("a").bind("touchstart", function () { }).bind("touchend", function () { });
+var $H = {
     conf:{
         version: "beta 2.0",
-        expiresTime: 600,//seconds
-        timeOut: 30,//seconds
         lazyLoadImg: "data:img/jpg;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==",
         loaderImg: "",
         plusCalendar: "",
         plusValidator: "hobbit.fn.validator.js"        
     },
-    initialize: function(){
-        //initializeBrowser();
-        //initializeLoader();
-    },
-    browser: {
-        isSafari: false,
-        isChrome: false,
-        isQQ: false,
-        isUc: false,
-        isIe: false,
-        ua: "",
-        height: 0,
-        width: 0,
-        jSon: { },//urlQueryStringToJson
-    },
+    /**
+    * Immediate Function pattern
+    */
+    browser: (function(){
+        var ua = navigator.userAgent.toLowerCase();
+        //var f = function(k) { return ua.indexOf() };
+        return {
+            ua: ua,
+            isSafari: ua.indexOf("safari") > 0 && ua.indexOf("chrome") <= 0,
+            isChrome: ua.indexOf("chrome") > 0,
+            isQQ: ua.indexOf("qqbrowser") > 0,
+            isUc: ua.indexOf("uc") > 0,//to do
+            isIe: ua.indexOf("ie") > 0,//to do
+            height: window.innerHeight || $(window).height(),
+            width: window.innerWidth || $(window).width(),
+        };
+    })(),
     loader: {
-        show:function(){},
-        hide:function(){},
+        ui: $("<div class='ui_loader'>").html("<div class='ui_loader_round'><div class='ui_loader_ring'></div></div><h1>努力加载中...</h1>"),
+        status: 0,
+        show: function () {
+            if ($H.loader.status == 0) {
+                $H.loader.ui.appendTo("body").show();
+                $H.loader.status = 1;
+            }
+        },
+        hide: function () {
+            if ($H.loader.status == 1) {
+                $H.loader.ui.remove();
+                $H.loader.status = 0;
+            }
+        }
     },
-    dialog: {},
+    dialog: {
+        ui: $("<div>"),
+        show: function(){ },
+        submit: function(){ },
+        cancle: function(){ }
+    },
     jSon: { 
         stringify: function(o, t){ },
         parse: function(s, t){},
+        
+        _stringifyJson:function(){},
+        _stringifyNvp:function(){},
+        _parseJsonStr:function(){},
+        _parseNvpStr:function(){},
     },
-    ajax: function(url, jSon, fn, et, to){
-        $h.loader.show();
+    /**
+     * @param {String} url
+     * @param {Object} jSon
+     * @param {Function} fn
+     * @param {number} et seconds
+     * @param {number} to seconds
+     * */ 
+    ajaxReq: function(url, jSon, fn, et, to){
+        $H.loader.show();
         //Expires
-        et = (et && et > 0) ? et : $h.conf.expiresTime;
-        to = (to && to > 0) ? to : $h.conf.timeOut;
-        var expires = parseInt(new Date().getTime() / (1000 * et));
+        et = (et && et > 0) ? et : 600;
+        to = (to && to > 0) ? to : 30;
+        var expires = parseInt(new Date().getTime() / (1000 * et), 10);
         //jSon.expires = 
         $.ajax({
             type: "GET",
             url: url,
             data: jSon,
             dataType: "json",
-            complete: function () { $h.loader.hide(); },
+            complete: function () { $H.loader.hide(); },
             success: function (data) {
                 fn(data);
-                $h.loader.hide();
+                $H.loader.hide();
             },
-            error: function () { $h.loader.hide(); }
+            error: function () { $H.loader.hide(); }
         });
     },
-    autoHeight: function(){},
-    storage:function(){},
+    /**
+     * Auto set page height，
+     */
+    autoPageHeight: function(){
+        //var body = $("body");
+        var browserHeight = $H.browser.height;
+        var bodyHeight = $("body").height();
+        var footerHeight = $("footer").height() ? $("footer").height() : 0;
+        $("body").height(browserHeight + 300);
+        $H.scrollTop();
+        browserHeight = $H.browser.height = window.innerHeight || $(window).height();
+        //footer style is "position:absolute; bottom:0"
+        bodyHeight = (bodyHeight + footerHeight < browserHeight) ? browserHeight : bodyHeight + footerHeight;
+        //bodyHeight = (bodyHeight < browserHeight) ? browserHeight : bodyHeight - footerHeight;
+        $("body").height(bodyHeight);
+    },
+    scrollTop: function(sel, callback){
+        //todo 
+        /*if($(sel).is("input") && $H.browser.isSafari){            
+            e.preventDefault && (e.preventDefault(), e.stopPropagation());
+        }*/
+        window.scrollTo(0, sel ? $(sel).offset().top : 1);
+        if(callback) callback();
+    },
+    storage: function(){},
+    /**
+     * klass
+     * @param {Object} parent
+     * @param {Object} props
+     */
+    klass: function(parent, props){
+        var Child, F, i;
+        
+        Child = function(){
+            if(Child.uber && Child.uber.hasOwnProperty("struct")){
+                Child.uber.struct.apply(this, arguments);
+            }
+            if(Child.prototype.hasOwnProperty("struct")){
+                Child.prototype.struct.apply(this, arguments);
+            }
+        };
+        
+        parent = parent || Object;
+        F = function(){};
+        F.prototype = parent.prototype;
+        Child.prototype = new F();
+        Child.uber = parent.prototype;
+        Child.prototype.constructor = Child;
 
-    xfn: function(url, fn){},
+        for(i in props){
+            if(props.hasOwnProperty(i)){
+                Child.prototype[i] = props[i];
+            }
+        }
 
-    ui: {
-        lable: function(id)  {
-            
-        },
-        checkbox: function() {},
-        radio: function() {},
-        select: function() {},
-        toolbar: function() {},
-        tabs: function() {},
-        accordion: function() {},
+        return Child;
+    },
 
-        xUi: function() {}
+    xfn: function(url, fn){
+        $H.loader.show();
+        $.loadScript(url, function(){
+            $H.loader.hide();
+            if(fn) fn();
+        }, true);
     }
 }
+
+var ui = $H.klass(null, {
+    struct: function(id){
+        this.id = id;
+    },
+    update: function(){}
+});
+
+var uiSpan = $H.klass(null,{
+    struct: function(id){
+        this.id = id;
+    },
+    show:function(){
+        
+    }
+ });
+
+var uiTabs = $H.klass(null, {
+    struct: function(id){
+        this.id = id;
+    },
+    show: function(){
+    
+    }
+});
+
+var uiDrawer = $H.klass(null, {
+    struct: function(sel){
+        this.sel = sel;
+        this.status = 0;
+    },
+    open: function(){
+        $(this.sel).show();
+        this.status = 1;
+    },
+    close: function(){
+        $(this.sel).hide();  
+        this.status = 0;  
+    },
+    toggle: function() {
+        if(this.status) this.close();
+        else this.open();
+    }
+});
+
+var uiNav = $H.klass(null, {
+    struct: function(sel){
+        $(sel).find("a").each(function(){ $(this).append("<span class='nav_arrow_r'></span>")});
+    }
+});
+
+
+
+
+
+
+
+
+
